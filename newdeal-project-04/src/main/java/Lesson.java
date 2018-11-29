8680d8debfbc998f0506d53e937c6ae48d2d292b
songseunghyeon <songing777@naver.com>
2018-11-29 10:45:16 +0900

    'v04'

---
+++ newdeal-project-04/src/main/java/Lesson.java
@@ -0,0 +1,16 @@
+import java.sql.Date;
+
+//새 데이터 타입
+//새로운 종류의 메모리
+//인스턴스 변수 와 스테틱 변수
+//인스턴스(메모리) 와 객체
+//클래스의 용도 : 데이터타입 정의, 메소드의 유지보수가 쉽게 분류
+public class Lesson {
+	int no;
+	String title;
+	String contents;
+	Date startDate;
+	Date endDate;
+	int totalHours;
+	int dayHours;
+}

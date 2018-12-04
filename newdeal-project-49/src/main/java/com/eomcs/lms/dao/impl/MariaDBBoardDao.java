f3353e3dcae512ecfceb318783e51ff4832e5467
songseunghyeon <songing777@naver.com>
2018-12-04 17:06:34 +0900

    'mybatis'

--- newdeal-project-49/src/main/java/com/eomcs/lms/dao/impl/MariaDBBoardDao.java
+++ newdeal-project-49/src/main/java/com/eomcs/lms/dao/impl/MariaDBBoardDao.java
@@ -6,124 +6,65 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.mariadb.jdbc.Driver;
 import com.eomcs.lms.dao.BoardDao;
 import com.eomcs.lms.domain.Board;
 
 public class MariaDBBoardDao implements BoardDao {
-  
-  public List<Board> findAll() throws Exception {
-    DriverManager.registerDriver(new Driver());
-    
-    try (
-        Connection con = DriverManager.getConnection(
-        "jdbc:mariadb://localhost:3306/studydb", 
-        "study", "1111");
-        PreparedStatement stmt = con.prepareStatement(
-            "select bno, cont, cdt, view from board");
-        ResultSet rs = stmt.executeQuery();) {
-      
-      ArrayList<Board> list = new ArrayList<>();
-      
-      while (rs.next()) {
-        Board board = new Board();
-        board.setNo(rs.getInt("bno"));
-        board.setContents(rs.getString("cont"));
-        board.setCreatedDate(rs.getDate("cdt"));
-        board.setViewCount(rs.getInt("view"));
-        
-        list.add(board);
-      }
-      
-      return list;
-    }
-  }
-  
-  public Board findByNo(int no) throws Exception {
-    DriverManager.registerDriver(new Driver());
-    try (
-        Connection con = DriverManager.getConnection(
-            "jdbc:mariadb://localhost:3306/studydb", 
-            "study", "1111");
-        PreparedStatement stmt = con.prepareStatement(
-            "select bno, cont, cdt, view, mno, lno"
-                + " from board where bno=?");) {
-      
-      stmt.setInt(1, no);
-      try (ResultSet rs = stmt.executeQuery()) {
-        if (rs.next()) {
-          Board board = new Board();
-          board.setNo(rs.getInt("bno"));
-          board.setContents(rs.getString("cont"));
-          board.setCreatedDate(rs.getDate("cdt"));
-          board.setViewCount(rs.getInt("view"));
-          board.setWriterNo(rs.getInt("mno"));
-          board.setLessonNo(rs.getInt("lno"));
-          return board;
-        } else {
-          return null;
-        }
-      }
-    }
-  }
-  
-  public int insert(Board board) throws Exception {
-    DriverManager.registerDriver(new Driver());
-    try (
-        Connection con = DriverManager.getConnection(
-        "jdbc:mariadb://localhost:3306/studydb", 
-        "study", "1111");
-        PreparedStatement stmt = con.prepareStatement(
-            "insert into board(cont,mno,lno) values(?,?,?)");) {
 
-      stmt.setString(1, board.getContents());
-      stmt.setInt(2, board.getWriterNo());
-      stmt.setInt(3, board.getLessonNo());
-      
-      return stmt.executeUpdate();
-    }
-  }
-  
-  public int update(Board board) throws Exception {
-    Connection con = null;
-    PreparedStatement stmt = null;
-    
-    try {
-      DriverManager.registerDriver(new Driver());
-      con = DriverManager.getConnection(
-          "jdbc:mariadb://localhost:3306/studydb", 
-          "study", "1111");
-      stmt = con.prepareStatement(
-          "update board set cont=? where bno=?");
-      
-      stmt.setString(1, board.getContents());
-      stmt.setInt(2, board.getNo());
-      return stmt.executeUpdate();
-      
-    } finally {
-      try {stmt.close();} catch (Exception e) {}
-      try {con.close();} catch (Exception e) {}
-    }
-  }
-  
-  public int delete(int no) throws Exception {
-    Connection con = null;
-    PreparedStatement stmt = null;
-    
-    try {
-      DriverManager.registerDriver(new Driver());
-      con = DriverManager.getConnection(
-          "jdbc:mariadb://localhost:3306/studydb", 
-          "study", "1111");
-      stmt = con.prepareStatement("delete from board where bno=?");
-      stmt.setInt(1, no);
-      return stmt.executeUpdate();
-      
-    } finally {
-      try {stmt.close();} catch (Exception e) {}
-      try {con.close();} catch (Exception e) {}
-    }
-  }
+	SqlSessionFactory sqlSessionFactory;
+	public MariaDBBoardDao(SqlSessionFactory sqlSessionFactory) {
+		this.sqlSessionFactory = sqlSessionFactory;
+	}
+
+	public List<Board> findAll() throws Exception {
+		try (SqlSession sqlSession = sqlSessionFactory.openSession();)
+		{
+
+			return sqlSession.selectList("BoardDao.findAll");
+		}
+	}
+
+	public Board findByNo(int no) throws Exception {
+		try (SqlSession sqlSession = sqlSessionFactory.openSession();)
+		 {
+
+		return sqlSession.selectOne("BoardDao.findByNo",no);
+	}
+	}
+
+	public int insert(Board board) throws Exception {
+		try (SqlSession sqlSession = sqlSessionFactory.openSession();)
+		{
+
+			int count = sqlSession.insert("BoardDao.insert",board);
+			sqlSession.commit();
+			return count;
+		}
+	}
+
+	public int update(Board board) throws Exception {
+		try (SqlSession sqlSession = sqlSessionFactory.openSession();)
+		{
+
+			int count = sqlSession.update("BoardDao.update",board);
+			sqlSession.commit();
+			return count;
+		}
+	}
+
+	public int delete(int no) throws Exception {
+		try (SqlSession sqlSession = sqlSessionFactory.openSession();)
+		{
+
+			int count = sqlSession.delete("BoardDao.delete",no);
+			sqlSession.commit();
+			return count;
+		}
+	}
 }
 
 
--- newdeal-project-49/src/main/java/com/eomcs/lms/dao/impl/MariaDBMemberDao.java
+++ newdeal-project-49/src/main/java/com/eomcs/lms/dao/impl/MariaDBMemberDao.java
@@ -4,39 +4,34 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.HashMap;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.mariadb.jdbc.Driver;
 import com.eomcs.lms.dao.MemberDao;
 import com.eomcs.lms.domain.Member;
 
 public class MariaDBMemberDao implements MemberDao {
 
-  @Override
-  public Member findByEmailPassword(String email, String password) throws Exception {
-    DriverManager.registerDriver(new Driver());
-    try (
-        Connection con = DriverManager.getConnection(
-            "jdbc:mariadb://localhost:3306/studydb", 
-            "study", "1111");
-        PreparedStatement stmt = con.prepareStatement(
-            "select mno,name,email,photo from member"
-            + " where email=? and pwd=?");) {
-      
-      stmt.setString(1, email);
-      stmt.setString(2, password);
-      
-      try (ResultSet rs = stmt.executeQuery()) {
-        if (rs.next()) {
-          Member member = new Member();
-          member.setNo(rs.getInt("mno"));
-          member.setName(rs.getString("name"));
-          member.setEmail(rs.getString("email"));
-          member.setPhoto(rs.getString("photo"));
-          return member;
-        } else {
-          return null;
-        }
-      }
-    }
-  }
+
+	SqlSessionFactory sqlSessionFactory;
+	public MariaDBMemberDao(SqlSessionFactory sqlSessionFactory) {
+		this.sqlSessionFactory = sqlSessionFactory;
+	}
+	@Override
+	public Member findByEmailPassword(String email, String password) throws Exception {
+		try (SqlSession sqlSession = sqlSessionFactory.openSession();)
+		{
+
+			HashMap<String,Object> params = new HashMap<>();
+			params.put("email", email);
+			params.put("password", password);
+
+			return sqlSession.selectOne("BoardDao.findByNo",params);
+
+		}
+		}
+		
 
 }
